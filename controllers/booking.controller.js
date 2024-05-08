09ec133e9b9ccd4efa5c82309d8df9ca11de89f1
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-05-08 17:54:34 +0530

    m

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,36 +1,6 @@
-
-
 const {viewBookingList} = require("../handlers/bookingHandler/viewBookingList");
 const {createBooking} = require('../handlers/bookingHandler/createBooking');
 const {updateBooking} = require('../handlers/bookingHandler/updateBooking')
-// exports.create_new_parking = [ 
-//   validateParking,
-//   handleValidationErrors,
-//   async (req, res) => {
-//   try {
-
-//     await createParking(req, res);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// }];
-
-// exports.handleUpdateParking = async (req, res) => {
-//   try {
-//     await updateParking(req, res);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// };
-
-// exports.handleDeleteParking = async (req, res) => {
-//   try {
-//     await deleteParking(req, res);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Internal server error' });
-//   }
-// };
-
 
 exports.view_booking_list = async (req, res) => {
   try {
--- routes/parking.route.js
+++ routes/parking.route.js
@@ -3,8 +3,10 @@ const router = require("express").Router();
 
   const parkingController = require("../controllers/parking.controller")
   
-  router.route('/api/parking/view-parking-list').get(parkingController.view_Parking_list);
+//   router.route('/api/parking/view-parking-list').get(parkingController.view_Parking_list);
   router.route('/api/parking/create-new-parking').post(parkingController.create_new_parking);
+  router.route('/search').get(parkingController.view_Parking_list);
+
 
 
   module.exports =router
