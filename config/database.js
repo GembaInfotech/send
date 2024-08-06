7ee810270e7956f8750a12df7de9f52d1b2f888b
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-08-06 11:44:17 +0530

    m

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
+
 class Database {
   constructor(uri, options) {
     this.uri = uri;
@@ -8,22 +9,21 @@ class Database {
 
   async connect() {
     try {
-      await mongoose.connect(this.uri, this.options);
-      console.log(
-        `Connected to database: ${mongoose.connection.db.databaseName}`
-      );
+      const connection = await mongoose.connect(this.uri, this.options);
+      console.log(`Connected to database: ${connection.connection.db.databaseName}`);
     } catch (error) {
+      console.error("Error connecting to database:", error);
       throw error;
     }
   }
 
   async disconnect() {
     try {
+      const dbName = mongoose.connection.db ? mongoose.connection.db.databaseName : "unknown";
       await mongoose.disconnect();
-      console.log(
-        `Disconnected from database: ${mongoose.connection.db.databaseName}`
-      );
+      console.log(`Disconnected from database: ${dbName}`);
     } catch (error) {
+      console.error("Error disconnecting from database:", error);
       throw error;
     }
   }
