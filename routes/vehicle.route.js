797c5fdb6dbea332f8f67c473620a96369d0aa49
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-08-14 16:15:57 +0530

    set vehcile as default api added

--- routes/vehicle.route.js
+++ routes/vehicle.route.js
@@ -11,7 +11,7 @@ router.route('/create-new-vehicle').post( decodeToken,  vehicleController.create
 
   router.route('/update-vehicle/:vehicleId').put( decodeToken, vehicleController.update_vehicle);
 
-  router.route('set-vehicle-default/:vehicleId').put( vehicleController.set_vehicle_default);
+  router.route('/set-vehicle-default/:vehicleId').put( vehicleController.set_vehicle_default);
 
   router.route('/view-vehicle-list').get(decodeToken, vehicleController.view_vehicle_list);
 
