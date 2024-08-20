9ddf45203c25b9f031f69dff49ebbae955b7c93c
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-08-20 16:17:06 +0530

    update vehicle api updated

--- handlers/VehicleHandler/setVehicleDefault.js
+++ handlers/VehicleHandler/setVehicleDefault.js
@@ -1,50 +1,56 @@
 const User = require('../../models/user.model');
 
 exports.setVehicleDefault = async (req, res) => {
-    const userId = req.userId; 
-    const vehicleId = req.params.vehicleId; 
-    const { vehicle_name, vehicle_number, vehicle_type } = req.body;
+    // const userId = req.userId; 
+    // const vehicleId = req.params.vehicleId; 
+    // const { vehicle_name, vehicle_number, vehicle_type } = req.body;
 
-    try {
-        const user = await User.findOne({_id:userId});
-        if (!user) {
-            return res.status(404).json({
-                success: false,
-                message: 'User not found',
-                data: null,
-            });
-        }
+    // try {
+    //     // Find the user by their ID
+    //     const user = await User.findOne({ _id: userId });
+    //     if (!user) {
+    //         return res.status(404).json({
+    //             success: false,
+    //             message: 'User not found',
+    //             data: null,
+    //         });
+    //     }
 
-        const vehicleIndex = user.vehicle.findIndex(vehicle => vehicle._id == vehicleId);
-        if (vehicleIndex === -1) {
-            return res.status(404).json({
-                success: false,
-                message: 'Vehicle not found',
-                data: null,
-            });
-        }
+    //     // Find the index of the vehicle to be updated
+    //     const vehicleIndex = user.vehicle.findIndex(vehicle => vehicle._id == vehicleId);
+    //     if (vehicleIndex === -1) {
+    //         return res.status(404).json({
+    //             success: false,
+    //             message: 'Vehicle not found',
+    //             data: null,
+    //         });
+    //     }
 
-        user.vehicle[vehicleIndex].vehicle_name = vehicle_name.toUpperCase();
-        user.vehicle[vehicleIndex].vehicle_number = vehicle_number.toUpperCase();
-        user.vehicle[vehicleIndex].vehicle_type = vehicle_type;
+    //     // Update the vehicle's information
+    //     user.vehicle[vehicleIndex].vehicle_name = vehicle_name.toUpperCase();
+    //     user.vehicle[vehicleIndex].vehicle_number = vehicle_number.toUpperCase();
+    //     user.vehicle[vehicleIndex].vehicle_type = vehicle_type;
 
-        user.vehicle.forEach(v => {
-            v.isDefault = v._id == vehicleId;
-        });
+    //     // Set all vehicles' isDefault to false, then set the selected one to true
+    //     user.vehicle.forEach(vehicle => {
+    //         vehicle.isDefault = vehicle._id == vehicleId;
+    //     });
 
-        await user.save();
+    //     // Save the updated user document
+    //     await user.save();
 
-        res.status(200).json({
-            success: true,
-            message: 'Vehicle set as default successfully',
-            data: user.vehicle[vehicleIndex],
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            success: false,
-            message: 'Internal server error',
-            data: null,
-        });
-    }
+    //     // Respond with success
+    //     res.status(200).json({
+    //         success: true,
+    //         message: 'Vehicle set as default successfully',
+    //         data: user.vehicle[vehicleIndex],
+    //     });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({
+    //         success: false,
+    //         message: 'Internal server error',
+    //         data: null,
+    //     });
+    // }
 };
--- handlers/VehicleHandler/updateVehicle.js
+++ handlers/VehicleHandler/updateVehicle.js
@@ -1,47 +1,56 @@
 const User = require('../../models/user.model');
 exports.updateVehicle = async (req, res) => {
-    console.log("Hello...")
-    const userId = req.userId; 
-    const vehicleId = req.params.vehicleId; 
-    const { vehicle_name, vehicle_number, vehicle_type } = req.body;
-  
-    try {
-      const user = await User.findOne({_id:userId});
+  const userId = req.userId; 
+  const vehicleId = req.params.vehicleId; 
+  const { vehicle_name, vehicle_number, vehicle_type } = req.body;
+
+  try {
+      // Find the user by their ID
+      const user = await User.findOne({ _id: userId });
       if (!user) {
-        return res.status(404).json({
-          success: false,
-          message: 'User not found',
-          data: null,
-        });
+          return res.status(404).json({
+              success: false,
+              message: 'User not found',
+              data: null,
+          });
       }
-  
+
+      // Find the index of the vehicle to be updated
       const vehicleIndex = user.vehicle.findIndex(vehicle => vehicle._id == vehicleId);
       if (vehicleIndex === -1) {
-        return res.status(404).json({
-          success: false,
-          message: 'Vehicle not found',
-          data: null,
-        });
+          return res.status(404).json({
+              success: false,
+              message: 'Vehicle not found',
+              data: null,
+          });
       }
-  
+
+      // Update the vehicle's information
       user.vehicle[vehicleIndex].vehicle_name = vehicle_name.toUpperCase();
       user.vehicle[vehicleIndex].vehicle_number = vehicle_number.toUpperCase();
       user.vehicle[vehicleIndex].vehicle_type = vehicle_type;
-  
-      await user.save();
-  
-      res.status(200).json({
-        success: true,
-        message: 'Vehicle updated successfully',
-        data: user.vehicle[vehicleIndex],
+
+      // Set all vehicles' isDefault to false, then set the selected one to true
+      user.vehicle.forEach(vehicle => {
+          vehicle.isDefault = vehicle._id == vehicleId;
       });
-    } catch (error) {
+
+      // Save the updated user document
+      await user.save();
+
+      // Respond with success
+      res.status(200).json({
+          success: true,
+          message: 'Vehicle set as default successfully',
+          data: user.vehicle[vehicleIndex],
+      });
+  } catch (error) {
       console.error(error);
       res.status(500).json({
-        success: false,
-        message: 'Internal server error',
-        data: null,
+          success: false,
+          message: 'Internal server error',
+          data: null,
       });
-    }
+  }
   };
   
