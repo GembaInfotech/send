5fef0f0d428944b54eab1388be3f6de5085b512b
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-11-12 12:36:16 +0530

    new build added

--- controllers/coupon.controller.js
+++ controllers/coupon.controller.js
@@ -1,45 +1,37 @@
 const Coupon = require('../models/CouponModel')
 const applyCoupon = async (req, res) => {
-    const { couponCode, parkingId, totalPrice } = req.body;
+    const { couponCode, parkingId, price } = req.body;
     const userId = req?.userId;
-  
+    console.log("req.body", req.body)
     try {
       const coupon = await Coupon.findOne({ code: couponCode });
   
       if (!coupon) {
         return res.status(404).json({ message: 'Invalid coupon code.' });
-      }
-  
-      // Check if the coupon is valid
+      }  
       const now = new Date();
       if (now < coupon.validFrom || now > coupon.validTill) {
         return res.status(400).json({ message: 'Coupon has expired or is not valid yet.' });
       }
-  
-      // Check if the coupon is applicable to this parking
       let isApplicable = coupon.isGlobal || coupon.applicableParkings.includes(parkingId);
       if (!isApplicable) {
         return res.status(400).json({ message: 'Coupon is not valid for this parking.' });
       }
   
-      // Check if the user has used this coupon
       const userUsage = coupon.usedBy.find((user) => user.userId.toString() === userId);
       if (userUsage && userUsage.usageCount >= coupon.usageLimitPerUser) {
         return res.status(400).json({ message: 'You have already used this coupon the maximum number of times.' });
       }
   
-      // Calculate discount
       let discount = 0;
       if (coupon.discountType === 'percentage') {
-        discount = (totalPrice * coupon.discountValue) / 100;
+        discount = (price * coupon.discountValue) / 100;
       } else if (coupon.discountType === 'fixed') {
         discount = coupon.discountValue;
       }
   
-      // Ensure total price doesn’t go below zero
-      const finalPrice = Math.max(totalPrice - discount, 0);
+      const finalPrice = Math.max(price - discount, 0);
   
-      // Update usage count for the user
       if (userUsage) {
         userUsage.usageCount += 1;
       } else {
@@ -47,7 +39,7 @@ const applyCoupon = async (req, res) => {
       }
   
       await coupon.save();
-  
+      console.log("result", finalPrice, discount )
       return res.json({
         message: 'Coupon applied successfully!',
         finalPrice,
