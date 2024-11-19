bb16a416302b2071344e836a277bb1f57f0e996a
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-11-19 10:49:36 +0530

    new build added

--- handlers/parkingHandler/getParkingByParkingId.js
+++ handlers/parkingHandler/getParkingByParkingId.js
@@ -1,7 +1,10 @@
 const ParkingModel = require('../../models/parking.model')
-
+const BookingModel = require('../../models/booking.model')
 exports.getParkingByParkingId = async (req, res) => {
   try {
+    console.log("req.body", req.query)
+    const inTime = req.query.inT;
+    const outTime = req.query.ouT;
     const {parkingId} = req.params; // Assuming the parking ID is passed in the URL params
     const parking = await ParkingModel.findOne({_id:parkingId});
     if (!parking) {
@@ -11,6 +14,31 @@ exports.getParkingByParkingId = async (req, res) => {
       });
     }
     
+    const countCar = await BookingModel.count({
+      parking: parkingId, 
+      inTime: { $lte: outTime }, 
+      outTime: { $gte: inTime },   
+      status: "Confirmed",
+      vehicle_type: "four wheeler"
+    });
+
+    const countBike = await BookingModel.count({
+      parking: parkingId, 
+      inTime: { $lte: outTime },  
+      outTime: { $gte: inTime },  
+      status: "Confirmed",
+      vehicle_type: "two wheeler"
+    });
+
+    parking.bookingData = {
+      countBike: countBike,
+      countCar: countCar,
+      inTime: inTime,
+      outTime: outTime,
+    };
+    
+    console.log("parkingdetails", parking);
+
     res.status(200).json({
       success: true,
       data: parking
