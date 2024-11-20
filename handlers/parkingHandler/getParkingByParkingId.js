8795a5a9d8300f31aede7169fd7e6308b16010d7
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-11-20 10:56:30 +0530

    get parking By parking id api added with carCount bikeCount

--- handlers/parkingHandler/getParkingByParkingId.js
+++ handlers/parkingHandler/getParkingByParkingId.js
@@ -6,14 +6,14 @@ exports.getParkingByParkingId = async (req, res) => {
     const inTime = req.query.inT;
     const outTime = req.query.ouT;
     const {parkingId} = req.params; // Assuming the parking ID is passed in the URL params
-    const parking = await ParkingModel.findOne({_id:parkingId});
+    let parking = await ParkingModel.findOne({_id:parkingId});
     if (!parking) {
       return res.status(404).json({
         success: false,
         message: 'Parking not found'
       });
     }
-    
+    parking = JSON.parse(JSON.stringify(parking));
     const countCar = await BookingModel.count({
       parking: parkingId, 
       inTime: { $lte: outTime }, 
@@ -30,12 +30,16 @@ exports.getParkingByParkingId = async (req, res) => {
       vehicle_type: "two wheeler"
     });
 
-    parking.bookingData = {
+    console.log("carCount", countCar, "bikeCount", countBike)
+    parking['bookingData'] = {
       countBike: countBike,
       countCar: countCar,
       inTime: inTime,
       outTime: outTime,
     };
+
+    console.log("bookingxyz", parking);
+    
     res.status(200).json({
       success: true,
       data: parking
